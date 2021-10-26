5a19d07fa99ebde012960f4ae7c659d9b8375507
Serg Mokhin <svmokhin@gmail.com>
2021-10-26 23:28:12 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,6 +1,13 @@
 package ru.job4j.grabber;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.TextNode;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 public class Post {
@@ -39,5 +46,37 @@ public class Post {
                 + ", description='" + description + '\''
                 + ", created=" + created
                 + '}';
+    }
+
+    private void getDescriptionFromUrl(String url) throws IOException {
+        int numberOfMsg = 0;
+
+        Document doc = Jsoup.connect(url).get();
+
+        List<TextNode> textMsgDescription = doc.select(".msgTable")
+                .get(numberOfMsg).select(".msgBody")
+                .get(1).textNodes();
+        StringBuilder sb = new StringBuilder();
+        for (TextNode tn : textMsgDescription) {
+            sb.append(tn.getWholeText());
+            sb.append(System.lineSeparator());
+        }
+        description = sb.toString().trim();
+
+        String wholeText = doc.select(".msgTable")
+                .get(numberOfMsg).select(".msgFooter")
+                .get(0).textNodes()
+                .get(0).getWholeText();
+        String data = wholeText.substring(0, wholeText.length() - 5).trim();
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        created = sqlRuDateTimeParser.parse(data);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Post post = new Post();
+        String url = "https://www.sql.ru/forum/"
+                + "1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
+        post.getDescriptionFromUrl(url);
+        System.out.println(post);
     }
 }
