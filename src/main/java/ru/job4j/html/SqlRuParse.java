45807c48f85c3eb6aabf4a358fa0281c26cd6ea4
Serg Mokhin <svmokhin@gmail.com>
2021-10-28 20:49:17 +0500

    2.3. Загрузка деталей поста. [#285212]+

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -17,61 +17,98 @@ import java.util.List;
 public class SqlRuParse implements Parse {
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
 
         List<Post> posts = new ArrayList<>();
 
         int amountOfPage = 1;
         for (int i = 1; i <= amountOfPage; i++) {
             String urlWithPage = link + "/" + i;
-            Document doc = Jsoup.connect(urlWithPage).get();
-            Elements row1 = doc.select(".postslisttopic");
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(urlWithPage).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Elements row1 = null;
+            if (doc != null) {
+                row1 = doc.select(".postslisttopic");
+            }
 
-            for (Element td : row1) {
-                Element href = td.child(0);
-                String ref = href.attr("href");
-                String title = href.text();
-                Post tmp = this.detail(ref);
-                tmp.setTitle(title);
-                posts.add(tmp);
+            if (row1 != null) {
+                for (Element td : row1) {
+                    Element href = td.child(0);
+                    String ref = href.attr("href");
+                    String title = href.text();
+                    Post tmp = this.detail(ref);
+                    tmp.setTitle(title);
+                    posts.add(tmp);
+                }
             }
         }
         return posts;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-
-        Document doc = Jsoup.connect(link).get();
+    public Post detail(String link) {
 
         int numberOfMsg = 0;
-        List<TextNode> textMsgDescription = doc.select(".msgTable")
-                .get(numberOfMsg).select(".msgBody")
-                .get(1).textNodes();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Element msgTable = null;
+        if (doc != null) {
+            msgTable = doc.select(".msgTable").get(numberOfMsg);
+        }
+
+        String title = null;
+        if (msgTable != null) {
+            title = msgTable.selectFirst(".messageHeader").text();
+        }
+
+        List<TextNode> textMsgDescription = null;
+        if (msgTable != null) {
+            textMsgDescription = msgTable.select(".msgBody")
+                    .get(1).textNodes();
+        }
         StringBuilder sb = new StringBuilder();
-        for (TextNode tn : textMsgDescription) {
-            sb.append(tn.getWholeText());
-            sb.append(System.lineSeparator());
+        if (textMsgDescription != null) {
+            for (TextNode tn : textMsgDescription) {
+                sb.append(tn.getWholeText());
+                sb.append(System.lineSeparator());
+            }
         }
         String description = sb.toString().trim();
 
-        String wholeText = doc.select(".msgTable")
-                .get(numberOfMsg).select(".msgFooter")
-                .get(0).textNodes()
-                .get(0).getWholeText();
-        String data = wholeText.substring(0, wholeText.length() - 5).trim();
+        String wholeText = null;
+        if (msgTable != null) {
+            wholeText = msgTable.select(".msgFooter")
+                    .get(0).textNodes()
+                    .get(0).getWholeText();
+        }
+        String data = null;
+        if (wholeText != null) {
+            data = wholeText.substring(0, wholeText.length() - 5).trim();
+        }
         SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
-        LocalDateTime created = sqlRuDateTimeParser.parse(data);
+        LocalDateTime created = null;
+        if (data != null) {
+            created = sqlRuDateTimeParser.parse(data);
+        }
 
         Post postTmp = new Post();
         postTmp.setLink(link);
+        postTmp.setTitle(title);
         postTmp.setDescription(description);
         postTmp.setCreated(created);
 
         return postTmp;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String url = "https://www.sql.ru/forum/job-offers";
         SqlRuParse sqlRuParse1 = new SqlRuParse();
         List<Post> posts = sqlRuParse1.list(url);
